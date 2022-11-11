acd775a64315491898794647c6e2310a38347495
Ripal Nathuji <rnathuji@neoteklabs.com>
2022-11-11 15:23:54 -0600

    Update plugin version for merge

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_raisecli';
-$plugin->version  = 2022101400;
+$plugin->version  = 2022111100;
 
 $plugin->dependencies = [
     'local_raise' => 2022102100
