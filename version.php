13c6c7c83dbf9e92acf13e63cc45ebc0752bdb2a
Ripal Nathuji <rnathuji@neoteklabs.com>
2023-03-10 16:01:33 -0600

    Update plugin version

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_raisecli';
-$plugin->version = 2023030100;
+$plugin->version = 2023031000;
 
 $plugin->dependencies = [
     'local_raise' => 2022102100
